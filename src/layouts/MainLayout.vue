d278672a58db5899b84d01d34b1029d3baf867e2
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-10 01:18:20 +0900

    feat: add `MainLayout` layout component
    #17

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,9 +5,9 @@
     </logo>
     <nav>
       <router-link to="/timetable">
-        <img src="" />
+        <v-icon icon="mdi:mdi-minus" />
       </router-link>
-      <img src="" />
+      <v-icon icon="mdi:mdi-minus" />
     </nav>
   </header>
   <main>
@@ -15,10 +15,43 @@
   </main>
 </template>
 
-<script>
-export default {
-  name: 'MainLayout',
-};
-</script>
+<script></script>
 
-<style></style>
+<style lang="scss" scoped>
+header {
+  border-bottom: 0.5px solid #ececec;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 16px;
+
+  logo {
+    margin-bottom: -6px;
+
+    a {
+      text-decoration: none;
+      color: #1f1f1f;
+      font-family: Montserrat;
+      font-weight: bold;
+      font-size: 20px;
+      line-height: 24px;
+    }
+  }
+
+  nav {
+    i {
+      width: 24px;
+      height: 24px;
+      background: gray;
+      margin-left: 13px;
+      cursor: pointer;
+    }
+  }
+}
+
+main {
+  padding: 0 16px;
+  height: calc(100vh - 46px);
+}
+</style>
